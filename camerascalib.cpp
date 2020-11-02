34f9c251f49c75144c6370496dd4e0d60efa0d05
Maoxu Li <maoxu@loopinno.com>
2020-11-02 16:18:56 -0500

    update code

--- camerascalib.cpp
+++ camerascalib.cpp
@@ -1,31 +1,3 @@
-/*
- * Copyright (c) 2019, NVIDIA CORPORATION. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *  * Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- *  * Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- *  * Neither the name of NVIDIA CORPORATION nor the names of its
- *    contributors may be used to endorse or promote products derived
- *    from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS ``AS IS'' AND ANY
- * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
- * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
- * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
- * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
- * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
- * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
 #include <iostream>
 #include <string>
 #include <sstream>
@@ -49,18 +21,22 @@ static std::string create_capture (int camera, int width, int height, int fps);
 
 static void help()
 {
-    std::cout << "\nThis is a sample OpenCV application to demonstrate "
-    "CSI-camera capture pipeline for NVIDIA accelerated GStreamer.\n\n"
-    "./opencv_nvgstcam [--Options]\n\n"
+    std::cout << "\nThis is a calibration tool running on Jetson Nano "
+    "or Jetson Xvier NX to generate transform between two CSI-cameras.\n\n" 
+    "./camerascalib [--Options]\n\n"
     "OPTIONS:\n"
     "\t-h,--help            Prints this message\n"
-    "\t--file               Calib file [Default = cameras.xml]\n"
-    "\t--width              Capture width [Default = 1280]\n"
-    "\t--height             Capture height [Default = 720]\n"
+    "\t--width              Capture width [Default = 1920]\n"
+    "\t--height             Capture height [Default = 1080]\n"
     "\t--fps                Frames per second [Default = 30]\n"
-    "\tq                    Runtime command to stop capture\n\n"
+    "\t--out                Output calibration (path and) filename [Default = cameras.xml]\n"
+    "\tc                    Runtime command to do a calibration\n"
+    "\ts                    Runtime command to save current transform\n"
+    "\tr                    Runtime command to reset (restart) calibration\n"
+    "\tq                    Runtime command to stop capture and quit\n\n"
+    "Example:\n"
+    "./camerascalib --width=1920 --height=1080 --fps=30 --out=/home/rose/cameras-1080p.xml\n\n"
     << std::endl;
-
 }
 
 static std::string create_capture (int camera, int width, int height, int fps)
@@ -84,14 +60,13 @@ void signal_callback_handler(int signum)
 
 int main(int argc, char const *argv[])
 {
+    int return_val = 0;
     std::string calib_file; 
     int width;
     int height;
     unsigned int fps;
-    int return_val = 0;
-    double fps_calculated;
-    cv::TickMeter ticks;
 
+    std::string pipeline0, pipeline1;
     cv::VideoCapture capture0, capture1;
     videostitcher::CamerasCalib::Settings calib_settings; 
     std::shared_ptr<videostitcher::CamerasCalib> calib; 
@@ -106,10 +81,10 @@ int main(int argc, char const *argv[])
 
     const std::string keys =
     "{h help         |              | message }"
-    "{file           |cameras.xml   | file }"
     "{width          |1920          | width }"
     "{height         |1080          | height }"
-    "{fps            |30            | frame per second }";
+    "{fps            |30            | frame per second }"
+    "{out            |cameras.xml   | output path and file name }";
 
     cv::CommandLineParser cmd_parser(argc, argv, keys);
 
@@ -119,7 +94,7 @@ int main(int argc, char const *argv[])
         goto cleanup;
     }
 
-    calib_file = cmd_parser.get<std::string>("file"); 
+    calib_file = cmd_parser.get<std::string>("out"); 
     width = cmd_parser.get<int>("width");
     height = cmd_parser.get<int>("height");
     fps = cmd_parser.get<unsigned int>("fps");
@@ -132,18 +107,22 @@ int main(int argc, char const *argv[])
         goto cleanup;
     }
 
-    capture0.open(create_capture(0, width, height, fps), cv::CAP_GSTREAMER);
+    pipeline0 = create_capture(0, width, height, fps);
+    capture0.open(pipeline0, cv::CAP_GSTREAMER);
     if (!capture0.isOpened())
     {
-        std::cerr << "Failed to open VideoCapture for first camera" << std::endl;
+        std::cerr << pipeline0 << std::endl; 
+        std::cerr << "Failed to open capture for first camera!" << std::endl;
         return_val = -4;
         goto cleanup;
     }
 
-    capture1.open(create_capture(1, width, height, fps), cv::CAP_GSTREAMER);
+    pipeline1 = create_capture(1, width, height, fps);
+    capture1.open(pipeline1, cv::CAP_GSTREAMER);
     if (!capture1.isOpened())
     {
-        std::cerr << "Failed to open VideoCapture for second camera" << std::endl;
+        std::cerr << pipeline1 << std::endl; 
+        std::cerr << "Failed to open capture for second camera!" << std::endl;
         return_val = -4;
         goto cleanup;
     }
@@ -153,7 +132,7 @@ int main(int argc, char const *argv[])
     calib_settings.match_mode = 0; 
     calib.reset(new videostitcher::CamerasCalib(calib_settings));
     if (!calib) {
-        std::cerr << "Failed to start calibrator" << std::endl;
+        std::cerr << "Failed to start calibrator!" << std::endl;
         return_val = -5;
         goto cleanup;
     }
@@ -167,11 +146,12 @@ int main(int argc, char const *argv[])
     cv::moveWindow(matches_window, 200, 100); 
     cv::moveWindow(warping_window, window_width + 250, 100); 
 
+    unsigned long frame_count; 
     g_stop = false;
     signal(SIGINT, signal_callback_handler);
     while (!g_stop)
     {
-        ticks.start();
+        // std::cout << "frame " << frame_count++ << std::endl; 
         capture0 >> images[0];
         capture1 >> images[1]; 
 
@@ -199,17 +179,7 @@ int main(int argc, char const *argv[])
         else if (key == 'r') {
             calib->Reset(); 
         }
-        ticks.stop();
     }
-
-    if (ticks.getCounter() == 0) {
-        std::cerr << "No frames processed" << std::endl;
-        return_val = -10;
-        goto cleanup;
-    }
-
-    fps_calculated = ticks.getCounter() / ticks.getTimeSec();
-    std::cout << "Fps observed " << fps_calculated << std::endl;
 
 cleanup:
     capture0.release();
